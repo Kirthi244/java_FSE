73de9c8d901b3f470dcee8ca4919a1a38bfa6a05
Kirthi244 <kirthika244@gmail.com>
2025-05-26 16:32:57 +0530

    Delete Module1 directory

--- Module1/module-1 java script/ex-1_javascript_setup/main.js
+++
@@ -1,7 +0,0 @@
-// Log message to console
-console.log("Welcome to the Community Portal");
-
-// Notify when the page has fully loaded
-window.onload = function() {
-  alert("Page is fully loaded!");
-};
